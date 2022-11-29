e6aa816532a5d11c7c61985aa833ca71e2278aef
Victorn65 <A01562528@tec.mx>
2022-11-29 10:51:19 -0600

    Agregar comentarios

--- ProyectoIoT5.2/ProyectoIoT5.2.ino
+++ ProyectoIoT5.2/ProyectoIoT5.2.ino
@@ -1,58 +1,85 @@
+// Librerias para la conexion de la pantalla lcd 16x2 con I2C
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+// Librerias para la conexion del NodeMCU a internet
 #include <ESP8266WiFi.h>
+// Librerias para la conexion del NodeMCU a un link receptor de datos
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
+// Libreria para la conexion del sensor Temp/Hum
 #include "DHT.h"
+// Definicion de pin en el GPIO del NodeMCU
 #define DHTPIN 5 // PIN D1
+// Definicion del tipo de sensor DHT en este caso el 22
 #define DHTTYPE DHT22
+// Declaracion para el tipo de variable del sensor
 DHT dht(DHTPIN, DHTTYPE);
-
-const char* ssid = "Iphone del guapo ese";  // Enter SSID here
-const char* password = "ladrando1";  //Enter Password here
+// Declaracion de variables para la conexion a wifi
+// Nota: solo admite red 2.4 
+const char* ssid = "Iphone del guapo ese";  // Nombre de la red
+const char* password = "ladrando1";  // Contrasena de la red
+// Declaracion de variable host que contiene parte del link
 const char *host = "http://thservers.herokuapp.com/api/data/get";// URL aqui
-
+// Definicion de variable del pin en el GPIO del NodeMCU del Buzzer
 int buz = 4; // GPIO13 PIN D2
-
+// Declaracion de variable para la lcd
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
-
+// Configuracion que se realiza unica vez cada encendido o reset del Hardware
 void setup() {
+  // Inicializacion de conexion del I2C 
   Wire.begin(2,0);
-  lcd.init();   // initializing the LCD
-  lcd.backlight(); // Enable or Turn On the backlight 
+  // Prendido de lcd
+  lcd.init();   
+  // Prendido de la luz del lcd
+  lcd.backlight();
+  // Funcion para limpiar de caracteres la pantalla
   lcd.clear();
-  lcd.setCursor(0, 0);
+  // Funcion para colocar el puntero para escribr en el lcd
+  lcd.setCursor(0, 0); // Cordenada en (x, y)
+  // Funcion para impresion de caracteres en la pantalla
   lcd.print("Iniciando...");
+  // Funcion para implementar una pausa en milisegundos
   delay(1000);
   lcd.setCursor(0, 1);
   lcd.print("Conectando WiFi!");
-  
+  // Declaracion del tipo de funcionamiento del pin, en este caso salida
   pinMode(buz, OUTPUT);
+  // Funcion para declarar lo que sale en el pin siendo:
+  // LOW = 0 volts
+  // HIGH = 3.3 volts
   digitalWrite(buz, LOW);
+  // Funcion para iniciar el puerto serie
   Serial.begin(115200);
+  // Funcion para imprimir en el puerto serie con un enter
   Serial.println();
   Serial.println();
   Serial.println("DHT22 Output!");
+  // Funcion para iniciar el DHT
   dht.begin();
   Serial.println("\nWiFi station setting");
-  WiFi.mode(WIFI_STA);
+  // Funcion necesaria para hacer funcionar la conexion a wifi
+  WiFi.mode(WIFI_STA);// Tipo de conexion de modo
+  // Funcion para iniciar la conexion a wifi
   WiFi.begin(ssid, password);
   Serial.println("Connecting...");
+  // Loop, Mientras que no conecte a internet muestra un punto en el puerto serie
   while(WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nWiFi ready");
   Serial.print("My ip address: ");
+  // Imprime el LocalIP de la conexion WiFi
   Serial.println(WiFi.localIP()); 
-
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Bienvenido!");  
   delay(2000);
 }
-
+// Funcion que implementa la alarma sonora
+// Recibe un string (ON / OFF)
 void peligro(String alarma){
+  // Si es on inicia la funcion sonora
   if (alarma == "ON"){
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -91,22 +118,31 @@ void peligro(String alarma){
     delay(250);
     digitalWrite(buz, HIGH);
   }
+  // En caso de no ser ON apaga el buzzer
   else{
     digitalWrite(buz, LOW);
   }
-  }
-
+}
+// Funcion que se repite indefinidamente mientras el dispositivo tenga energia
 void loop() {
+  // Creacion de variables
   String alarma;
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();  
+  float temperature = dht.readTemperature();// Funcion para obtener el valor de temp del DHT
+  float humidity = dht.readHumidity();// Funcion para obtener el valor de hum del DHT
+  // Condicional que pregunta si existen valores del sensor o no
+  // En caso de desconectar el sensor aparecen valores nan
+  // En caso no estar funcionando el sensor aparecen valores nan
   if(isnan(temperature) || isnan(humidity)){
+    // En caso de tener valores nan: -->
     Serial.println("Failed to read DHT22");
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Failed DHT22");
+  // En caso de existir valores: -->
   }else{
-    if (temperature > 26.00 || humidity > 70.00){
+    // Dependiendo de los valores checa si hay un sobre paso de los declarados
+    // para saber si inicia la alarma
+    if (temperature > 26.00 || humidity > 70.00){ // Rangos para la alarma
       alarma = "1";
       peligro("ON");
     }
@@ -114,6 +150,7 @@ void loop() {
       alarma = "0";
       peligro("OFF");
     }
+    // Inicia la impresion de valores en pantalla y puerto
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("T=");
@@ -134,32 +171,41 @@ void loop() {
     Serial.println(" *C");
     delay(500);
   }
-  if(WiFi.status() != WL_CONNECTED){
+  // Pregunta si hay conexion a internet
+  if(WiFi.status() != WL_CONNECTED){// Si no hay conexion entonces:
     lcd.setCursor(0, 1);
     lcd.print("WiFi=OFF");
     Serial.println("WiFi OFF");
   }
-  else{
+  else{// Si hay conexion entonces: 
     lcd.setCursor(0, 1);
     lcd.print("WiFi=ON ");
     Serial.println("WiFi ON");
   }
+  // Inicializamos los tipos de variable de conexion a nuestro host
   WiFiClient client;
   HTTPClient http;
-  
+  // Primera parte del link = host
   //http://thservers.herokuapp.com/api/data/get
+  // Variables para creacion de link completo
   String getData, link;
+  // Variable que se compone de la estructura necesaria para mandar los datos al server
   getData = ("?temperature=") + String(temperature) + ("&humidity=") + String(humidity) + ("&alarm=") + String(alarma);
+  // Agrupacion de los strings para la cracion del link completo
   link = host + getData;
-
+  // Inicializacion de conexion al servidor con envio de datos
   http.begin(client, link);
+  // Variable para reconocer el codigo de conexion
   int httpCode = http.GET();
+  // Impresion del Response code
   lcd.setCursor(9, 1);
   lcd.print("RC=");
   lcd.setCursor(12, 1);
   lcd.print(httpCode);
   Serial.print("Response Code: ");
   Serial.println(httpCode);
+  // Cerrado de conexion al servidor para no tener errores o problemas
   http.end();
-  delay((1000*60));
+  // Pausa de tiempo de 60 Segundos 
+  delay((1000*60));// Cambiar dependiendo de la necesidad de datos
 }
